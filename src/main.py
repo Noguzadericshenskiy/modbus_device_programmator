625a8098805f11fd995297eda8186ccf47bf4831
Гридин Роман <grg@sigma-is.ru>
2023-09-28 15:04:06 +0300

    add functions change_parameters, get_info

--- src/main.py
+++ src/main.py
@@ -3,7 +3,8 @@ from pymodbus.client import ModbusSerialClient
 from devices import devases_setup
 
 
-def get_info(port: str, params_def: dict):
+def get_info(port: str, params_def: dict) -> dict:
+    info = {}
 
     client = ModbusSerialClient(
         port=port,
@@ -12,11 +13,30 @@ def get_info(port: str, params_def: dict):
         timeout=2
     )
     client.connect()
-    data = client.read_holding_registers(address=, slave=params_def["slave"])
-    data = client.read_holding_registers(address=, slave=params_def["slave"])
-    data = client.read_holding_registers(address=, slave=params_def["slave"])
-    data = client.read_holding_registers(address=, slave=params_def["slave"])
+    info["slave"] = client.read_holding_registers(address=params_def["register_slave"], slave=params_def["slave"])
+    info["baudrate"] = client.read_holding_registers(address=params_def["register_baudrate"], slave=params_def["slave"])
+    info["stopbit"] = client.read_holding_registers(address=params_def["register_stopbit"], slave=params_def["slave"])
+    info["stopbits"] = client.read_holding_registers(address=params_def["register_stopbits"], slave=params_def["slave"])
+
     client.close()
-    return
+    return info
+
+
+def change_parameters(port: str, address: int, params_def: dict) -> str:
+    client = ModbusSerialClient(
+        port=port,
+        baudrate=params_def["baudrate"],
+        framer=pymodbus.framer.ModbusRtuFramer,
+        timeout=0.5
+    )
+    client.connect()
+    client.write_registers(
+        address=params_def["register_slave"],
+        values=[address, 9600, 1, 1],
+        slave=params_def["slave"]
+    )
+    client.close()
+
+    return "ok"
 
 
---
+++ src/view.py
@@ -0,0 +1,3 @@
+
+
+from src.devices import devases_setup
